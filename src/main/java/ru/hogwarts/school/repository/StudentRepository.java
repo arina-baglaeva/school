2d837bae707d1137da9d2d18ef134b4fc9abae81
ariba <ariba2576@gmail.com>
2026-06-23 21:57:50 +0300

    Module (work with code. advanced level) 5 hm

--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -1,5 +1,6 @@
 package ru.hogwarts.school.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import ru.hogwarts.school.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,15 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findByAge(int age);
 
     List<Student> findByAgeBetween(int minAge, int maxAge);
+
+    List<Student> findByFacultyId(Long facultyId);
+
+    @Query(value = "SELECT COUNT(*) FROM student", nativeQuery = true)
+    Integer getStudentCount();
+
+    @Query(value = "SELECT AVG(age) FROM student", nativeQuery = true)
+    Double getAverageAge();
+
+    @Query(value = "SELECT * FROM student ORDER BY id DESC LIMIT 5", nativeQuery = true)
+    List<Student> findLastFiveStudents();
 }
--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -1,10 +1,16 @@
 package ru.hogwarts.school.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import ru.hogwarts.school.model.Avatar;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
 
 public interface AvatarService {
     void uploadAvatar(Long studentId, MultipartFile file) throws IOException;
+
     Avatar findAvatar(Long studentId);
+
+    Page<Avatar> findAll(Pageable pageable);
 }
